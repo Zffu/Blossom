923a6b846be4f1fe709590b8a97438150d3844c5
Zffu <radi0o0@outlook.com>
2024-02-10 01:08:45 +0100

    - Moved down group token related functions to GroupToken.h

---
+++ GroupToken.h
@@ -0,0 +1,18 @@
+#ifndef BLOSSOM_TOOLCHAIN_LEXAR_GROUP_TOKEN
+#define BLOSSOM_TOOLCHAIN_LEXAR_GROUP_TOKEN
+
+/*
+The Group Tokens of the Blossom Language.
+Only contains things that are definitive symbols.
+*/
+
+enum GroupTokenTypes {
+	MAIN,
+	BRACKETS,
+	PARENTHESIS,
+	LIST,
+
+};
+
+
+#endif
