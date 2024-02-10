12148aad8f76a709608993e4ea7cd300fa4c224d
Zffu <radi0o0@outlook.com>
2024-02-10 01:05:09 +0100

    - Moved Token::Type to TokenType.

--- toolchain/lexar/Token.h
+++ toolchain/lexar/Token.h
@@ -9,43 +9,43 @@ Only contains things that are definitive symbols.
 #include<vector>
 #include<string>
 
+enum TokenType {
+
+	// Dynamically assigned types / Internal Types
+	NONE = -1,
+	STRING_LITTERAL = -2,
+	NUMBER_LITTERAL = -3,
+	ENDOFFILE = -4,
+
+	// Grouping Types
+	BRACKET_OPEN = 1,
+	BRACKET_CLOSE = 2,
+	PARENTHESIS_OPEN = 3,
+	PARENTHESIS_CLOSE = 4,
+	LIST_OPEN = 5,
+	LIST_CLOSE = 6,
+
+	// Misc Types
+	DOT = 7,
+	SEMICOLON = 8,
+
+	STRING_UNKNOWN = 9,
+
+	STRING_OPENER = 10
+};
+
 class Token {
 public:
 
-	enum Type {
-
-		// Dynamically assigned types / Internal Types
-		NONE = -1,
-		STRING_LITTERAL = -2,
-		NUMBER_LITTERAL = -3,
-		ENDOFFILE = -4,
-
-		// Grouping Types
-		BRACKET_OPEN = 1,
-		BRACKET_CLOSE = 2,
-		PARENTHESIS_OPEN = 3,
-		PARENTHESIS_CLOSE = 4,
-		LIST_OPEN = 5,
-		LIST_CLOSE = 6,
-
-		// Misc Types
-		DOT = 7,
-		SEMICOLON = 8,
-
-		STRING_UNKNOWN = 9,
-
-		STRING_OPENER = 10
-	};
-
-	Type type;
+	TokenType type;
 	string raw;
 
-	Token(Type t, string r) {
+	Token(TokenType t, string r) {
 		type = t;
 		raw = r;
 	}
 
-	void setType(Type t) {
+	void setType(TokenType t) {
 		type = t;
 	}
 
