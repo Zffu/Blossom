b0d5808084b5783b6cab5c7fb3d8c81d897e4d6f
Zffu <radi0o0@outlook.com>
2024-02-26 10:16:28 +0100

    Removed stuff that will be replaced by LLVM

--- env/internal/Internal.cpp
+++
@@ -1,9 +0,0 @@
-#include "../internal/Internal.h"
-#include "../objects/Object.h"
-#include "../ctx/MethodContext.h"
-#include <iostream>
-
-Object PrintMethod::run(MethodContext ctx) {
-	std::cout << "todo: Add Args";
-	return Object();
-}
